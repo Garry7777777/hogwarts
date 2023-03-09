98fb02402b94bd07c888e336ff615e5aec35f212
Garry7777777 <121170491+Garry7777777@users.noreply.github.com>
2023-03-09 11:49:04 +0300

    Урок 9. Введение в базы данных

--- src/main/java/ru/hogwarts/school/service/HouseService.java
+++ src/main/java/ru/hogwarts/school/service/HouseService.java
@@ -11,10 +11,23 @@ public class HouseService {
 
     @Autowired
     private FacultyRepository facultyRepository;
-    public Faculty createFaculty(Faculty faculty){return facultyRepository.save(faculty);}
-    public Faculty findFaculty(long id){return facultyRepository.findById(id).get();}
-    public Faculty editFaculty(Faculty faculty){return facultyRepository.save(faculty);}
-    public void deleteFaculty(Long id){facultyRepository.deleteById(id);}
-    public Collection<Faculty> getFaculties(){return facultyRepository.findAll();}
-    public Collection<Faculty> getFacultyByColor(String color){return facultyRepository.findByColor(color);}
+    
+    public Faculty createFaculty(Faculty faculty){
+        return facultyRepository.save(faculty);
+    }
+    public Faculty findFaculty(long id){
+        return facultyRepository.findById(id).get();
+    }
+    public Faculty editFaculty(Faculty faculty){
+        return facultyRepository.save(faculty);
+    }    
+    public void deleteFaculty(Long id){
+        facultyRepository.deleteById(id);
+    }    
+    public Collection<Faculty> getFaculties(){
+        return facultyRepository.findAll();
+    }
+    public Collection<Faculty> getFacultyByColor(String color){
+        return facultyRepository.findByColor(color);
+    }
 }
