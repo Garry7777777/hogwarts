a844e8404f690d1006894e13b9adb2f9c0b191bd
Garry7777777 <garry4978@gmail.com>
2023-03-17 00:45:11 +0300

    Урок 14. Логирование и конфигурирование в приложении

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,11 +1,8 @@
 package ru.hogwarts.school.controller;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appinfo")
