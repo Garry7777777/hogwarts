b9cfc403153b3a619064fdcd75b55f5b25a67cd8
Garry7777777 <garry4978@gmail.com>
2023-03-17 00:03:47 +0300

    Урок 14. Логирование и конфигурирование в приложении

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.school.model.AppInfo;
 
@@ -7,8 +8,15 @@ import ru.hogwarts.school.model.AppInfo;
 @RequestMapping("/appinfo")
 public class InfoController {
 
+    @Value("${app.info.name: hogwarts-school}")
+    private String name ;
+    @Value("${app.info.version: 0.0.1}")
+    private String version;
+    @Value("${app.info.environment}")
+    private String environment;
+
     @GetMapping
     public AppInfo getAppInfo(){
-        return new AppInfo();
+        return new AppInfo(name, version, environment);
     }
 }
--- src/main/java/ru/hogwarts/school/model/AppInfo.java
+++ src/main/java/ru/hogwarts/school/model/AppInfo.java
@@ -1,14 +1,11 @@
 package ru.hogwarts.school.model;
 
 import lombok.*;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
+@AllArgsConstructor
 public class AppInfo {
-    @Value("${app.name: hogwarts-school}")
-    private String appName;
-    @Value("${app.version: 0.0.1}")
+    private String appName ;
     private String appVersion;
-    @Value("${app.env}")
     private String appEnvironment;
 }
